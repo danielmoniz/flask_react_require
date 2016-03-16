408b5731b2a3cbde5eef40a4dbe017e878d47f03
Daniel Moniz <daniel.moniz@gmail.com>
2016-03-16 12:31:13 -0400

    Get very basic React project working with requirejs.

---
+++ static/main.jsx
@@ -0,0 +1,11 @@
+define(function(require, exports, module) {
+  var React = require("react")
+  
+  module.exports = React.createClass({
+    render: function() {
+      return (
+        <h1>React.js FTW</h1>
+      )
+    }
+  })
+})
